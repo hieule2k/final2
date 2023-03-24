778ebdfd91389a3d6e5684852e6e7244be6e124c
LeHuyenLinh-20187178-HUST <linhhuyen.2k@gmail.com>
2023-03-24 12:43:48 +0700

    css search page

--- FE/src/components/Search-bar/Search-bar.js
+++ FE/src/components/Search-bar/Search-bar.js
@@ -1,55 +1,55 @@
-import React, { useState } from "react";
-import styles from "./Search-bar.module.css";
+import React, { useState } from 'react';
+import styles from './Search-bar.module.css';
 // import search from "../../assets/img/search-icon.png";
-import classNames from "classnames/bind";
-import "react-datepicker/dist/react-datepicker.css";
-import SearchField from "./SearchField";
+import classNames from 'classnames/bind';
+import 'react-datepicker/dist/react-datepicker.css';
+import SearchField from './SearchField';
 const cx = classNames.bind(styles);
 
 function SearchBar() {
-  const myObject = ["Apartments", "Rooms", "Villas"];
+    const myObject = ['Apartments', 'Rooms', 'Villas'];
 
-  // const [type, setType] = useState(myObject[0]);
-  const [state, changeState] = useState({
-    activeObject: myObject[0],
-  });
+    // const [type, setType] = useState(myObject[0]);
+    const [state, changeState] = useState({
+        activeObject: myObject[0],
+    });
 
-  function handleClick(index, element) {
-    changeState({ ...state, activeObject: myObject[index] });
-    // setType(element);
-  }
-
-  function toggleActive(index) {
-    if (myObject[index] === state.activeObject) {
-      return cx("option-item", "active");
-    } else {
-      return cx("option-item");
+    function handleClick(index, element) {
+        changeState({ ...state, activeObject: myObject[index] });
+        // setType(element);
     }
-  }
 
-  return (
-    <div className={cx("search-bar")}>
-      <div className={cx("find")}>
-        <span>FIND</span>
-      </div>
-      <div className={cx("search-option")}>
-        <ul className={cx("option-lists")}>
-          {myObject.map((element, index) => (
-            <li
-              key={index}
-              className={toggleActive(index)}
-              onClick={() => {
-                handleClick(index, element);
-              }}
-            >
-              {element}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <SearchField></SearchField>
-    </div>
-  );
+    function toggleActive(index) {
+        if (myObject[index] === state.activeObject) {
+            return cx('option-item', 'active');
+        } else {
+            return cx('option-item');
+        }
+    }
+
+    return (
+        <div className={cx('search-bar')}>
+            <div className={cx('find')}>
+                <span>FIND</span>
+            </div>
+            <div className={cx('search-option')}>
+                <ul className={cx('option-lists')}>
+                    {myObject.map((element, index) => (
+                        <li
+                            key={index}
+                            className={toggleActive(index)}
+                            onClick={() => {
+                                handleClick(index, element);
+                            }}
+                        >
+                            {element}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <SearchField></SearchField>
+        </div>
+    );
 }
 
 export default SearchBar;
