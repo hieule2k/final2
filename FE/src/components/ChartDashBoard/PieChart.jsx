1031a48aae29bb9b44547fa09e819c055ea854df
LeHuyenLinh-20187178-HUST <linhhuyen.2k@gmail.com>
2023-05-12 19:40:06 +0700

    Dashboard

---
+++ FE/src/components/ChartDashBoard/PieChart.jsx
@@ -0,0 +1,5 @@
+// import React from 'react';
+// import HighchartsReact from 'highcharts-react-official';
+
+// const PieChart = ({ options, highcharts }) => <HighchartsReact highcharts={highcharts} constructorType={'chart'} options={options} />;
+// export default PieChart;
